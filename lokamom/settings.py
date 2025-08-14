0c25b349848f90ed40781cf601cea13abbe75c56
Kiran Sindagi <120112796+kiran-sindagi@users.noreply.github.com>
2025-08-14 17:22:29 +0530

    Update settings.py

--- lokamom/settings.py
+++ lokamom/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
         'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT', '3306'),
+        'PORT': os.environ.get('DB_PORT', '4174'),
         'OPTIONS': {
             'charset': 'utf8mb4',
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
